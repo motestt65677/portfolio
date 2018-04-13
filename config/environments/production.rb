250c94c0f078374e4de1bfd242f22c59e19d8e1c
motestt65677 <pkpk99887766@gmail.com>
2018-04-13 17:08:02 +0800

    third commit fixed email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,17 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "https://DavidCheng.herokuapp.com/"}
+  config.action_mailer.default_url_options = { :host => "https://davidcheng.herokuapp.com/"}
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
+
 end
